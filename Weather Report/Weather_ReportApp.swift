345b4b3fac63097d6dbe1c8473eb9d546dda5c3e
Pavlo Osypov <blackgreenenator@gmail.com>
2023-07-27 16:55:47 +0300

    Initial Commit

---
+++ Weather Report/Weather_ReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ReportApp.swift
+//  Weather Report
+//
+//  Created by MacBook on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
