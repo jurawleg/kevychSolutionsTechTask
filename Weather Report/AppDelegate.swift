94c6b95e5ba69fccd3b86dc508bee263c95b7fe9
Pavlo Osypov <blackgreenenator@gmail.com>
2023-07-27 17:07:20 +0300

    Add appDelegate file.

---
+++ Weather Report/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Weather Report
+//
+//  Created by MacBook on 27.07.2023.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+}
